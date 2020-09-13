2d3e2c37ce3d6dfc30c6a473303c296930172b82
Cai <guoyu.cai@accenture.com>
2020-09-13 14:36:36 +0800

    使用spring来创建bean

---
+++ new/spring_chushi_test/spring_day001/day01_eesy_03spring/src/main/java/com/itheima/ui/Client.java
@@ -0,0 +1,17 @@
+package com.itheima.ui;
+
+import com.itheima.dao.IAccountDao;
+import com.itheima.service.IAccountService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 模拟一个表现层，用于调用业务层
+ */
+public class Client {
+
+   public static void main(String[] args) {
+	   IAccountService as=new AccountServiceImpl();
+	   as.saveAccount();
+   }
+}
